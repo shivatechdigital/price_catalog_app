3f68d536c54602a092bc816d1a37d46344eac6d8
TechStorm <techstorm@TechStorms-MacBook-Pro.local>
2026-07-11 15:46:22 +0530

    update google json file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+    // Apply the Google Services plugin (Firebase / google-services.json)
+    id("com.google.gms.google-services")
 }
 
 android {
